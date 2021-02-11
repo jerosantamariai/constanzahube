1b40b6c218350bb1cf8b287072ada6fd94dd8d5b
Jerónimo Santa María Illanes <jerosantamariai@MacBook-Pro-de-Jeronimo.local>
2021-02-11 14:47:23 -0300

    underconstruction ready with navbar and footer

--- front/src/components/footer.jsx
+++ front/src/components/footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Footer = props => {
     return (
-        <h1>Footer</h1>
+        <footer className="d-flex justify-content-center">
+            <a href="www.instagram.com/constanzahube.constituyente" target="_blank"><i className="fab fa-instagram fa-3x mr-4"></i></a>
+            <a href="www.facebook.com/constanzahube.constituyente" target="_blank"><i className="fab fa-facebook fa-3x mr-4"></i></a>
+            <a href="www.twitter.com/conihube" target="_blank"><i className="fab fa-twitter-square fa-3x"></i></a>
+        </footer>
     );
 }
 
