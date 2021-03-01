95cf97c011397f2a99db60c7745f95828ad4098a
jerosantamariai <jerosantamariai@gmail.com>
2021-03-01 10:35:32 -0300

    fixing navbar problems

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = props => {
         <nav className="navbar navbar-expand-lg fixed-top">
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    {/* <span className="navbar-toggler-icon"></span> */}
+                    <i class="fas fa-bars"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav mx-auto">
